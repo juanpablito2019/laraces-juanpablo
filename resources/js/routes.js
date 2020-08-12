c6d01e7b2315947e46bd27f745977b68af2336d7
Santiago Bedoya <santiagobedoya@MacBook-Air-de-Santiago.local>
2020-08-11 21:44:46 -0500

    Fix sidebar

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -13,110 +13,110 @@ import Modalities from "./pages/Modalities";
 import Groups from "./pages/Groups";
 export default [
     {
-        name:'Home',
-        path:'/',
-        component:Home
+        name: 'Home',
+        path: '/',
+        component: Home
     },
     {
-        name: 'Parametros comité',
-        type:'menu',
-        routes:[
+        name: 'Parámetros comité',
+        type: 'menu',
+        routes: [
             {
-                name:'Sanciones',
-                path:'/sanctions',
-                component:Sanctions
+                name: 'Parametros de acta',
+                path: '/committee-parameters',
+                component: CommitteeParameters
             },
             {
-                name:'Parametros de acta',
-                path:'/committee-parameters',
-                component:CommitteeParameters
+                name: 'Sanciones',
+                path: '/sanctions',
+                component: Sanctions
             },
             {
-                name:'Tipos de casos',
-                path:'/committee-session-types',
-                component:CommitteeSessionTypes
+                name: 'Medidas formativas',
+                path: '/formative-measures',
+                component: FormativeMeasures
             },
             {
-                name:'Medidas formativas',
-                path:'/formative-measures',
-                component:FormativeMeasures
+                name: 'Clasificacion de las faltas',
+                path: '/infringement-classifications',
+                component: Learners
             },
             {
-                name:'Tipos de novedades',
-                path:'/novelty-types',
-                component:Learners
+                name: 'Tipos de faltas',
+                path: '/infringement-types',
+                component: Learners
             },
             {
-                name:'Tipos de faltas',
-                path:'/infringement-types',
-                component:Learners
+                name: 'Tipos de novedades',
+                path: '/novelty-types',
+                component: Learners
             },
             {
-                name:'Clasificacion de las faltas',
-                path:'/infringement-classifications',
-                component:Learners
+                name: 'Tipos de casos',
+                path: '/committee-session-types',
+                component: CommitteeSessionTypes
             },
         ]
     },
     {
-        name:'Parametros generales',
+        name: 'Parámetros generales',
         type: 'menu',
-        routes:[
+        routes: [
             {
-                name:'Cargos',
-                path:'/positions',
-                component:Positions
+                name: 'Cargos',
+                path: '/positions',
+                component: Positions
             },
             {
-                name:'Tipos de contratos',
-                path:'/contract-types',
-                component:ContractTypes
+                name: 'Tipos de contratos',
+                path: '/contract-types',
+                component: ContractTypes
             },
             {
-                name:'Modalidades',
-                path:'/modalities',
-                component:Modalities
+                name: 'Modalidades',
+                path: '/modalities',
+                component: Modalities
             },
             {
-                name:'Programas de formacion',
-                path:'/formation-programs',
-                component:FormationPrograms
+                name: 'Programas de formacion',
+                path: '/formation-programs',
+                component: FormationPrograms
             },
             {
-                name:'Tipos de programas de formacion',
-                path:'/formation-program-types',
+                name: 'Tipos de programas de formacion',
+                path: '/formation-program-types',
                 component: FormationProgramTypes
             },
         ]
     },
     {
-        name:'Aprendices',
-        path:'/learners',
-        component:Learners
+        name: 'Aprendices',
+        path: '/learners',
+        component: Learners
     },
-    
+
     {
-        name:'Grupos',
-        path:'/groups',
-        component:Groups
+        name: 'Grupos',
+        path: '/groups',
+        component: Groups
     },
-    
-    
+
+
     {
-        name:'Comités',
-        path:'/committees',
-        component:Learners
+        name: 'Comités',
+        path: '/committees',
+        component: Learners
     },
-    
+
     {
-        name:'Novedades del aprendiz',
-        path:'/learner-novelties',
-        component:Learners
+        name: 'Novedades del aprendiz',
+        path: '/learner-novelties',
+        component: Learners
     },
-    
+
     {
-        name:'Responsables de medida formativa',
-        path:'/formative-measure-responsibles',
-        component:ResponsiblesFormativeMeasures
+        name: 'Responsables de medida formativa',
+        path: '/formative-measure-responsibles',
+        component: ResponsiblesFormativeMeasures
     },
 ]
