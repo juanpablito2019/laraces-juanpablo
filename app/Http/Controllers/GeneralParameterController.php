d3847543a1d04563caeb9cf7cba9dd8629bc62b5
Santiago Bedoya <santiagobedoya@MacBook-Air-de-Santiago.local>
2020-09-01 21:13:22 -0500

    Acts management

---
+++ app/Http/Controllers/GeneralParameterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GeneralParameter;
+use Illuminate\Http\Request;
+
+class GeneralParameterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GeneralParameter  $generalParameter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GeneralParameter $generalParameter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\GeneralParameter  $generalParameter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GeneralParameter $generalParameter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GeneralParameter  $generalParameter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GeneralParameter $generalParameter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GeneralParameter  $generalParameter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GeneralParameter $generalParameter)
+    {
+        //
+    }
+}
