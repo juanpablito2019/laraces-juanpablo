454c3b1c5e77b99769e33eb91d1aa2ede78e09e9
Johanquintero <quinteroquinte@hotmail.com>
2020-10-05 15:37:55 -0500

    sebas pull

--- resources/js/pages/Users.js
+++ resources/js/pages/Users.js
@@ -1,20 +1,119 @@
 import React, { Component } from 'react';
 import Loader from '../components/Loader';
-import { get } from '../containers/User';
+import { validate, formValid, setRules } from '../containers/Validator';
+import { get, store, find, update, destroy, rules} from '../containers/User';
+import { get as getRoles } from '../containers/Roles';
 
 class Users extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: null
+            users: null,
+            edit: false,
+            roles: [],
+            id: null,
+            message: null,
+            rules: rules
         }
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleModal = this.handleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     }
     async getUsers() {
         let data = await get();
         this.setState({ users: data });
+
     }
+
+    async getRoles() {
+        let data = await getRoles();
+        this.setState({ roles: data });
+    }
+
+    handleInput(e) {
+        let { name, value } = e.target;
+        let newRules = validate(name, value, rules)
+        this.setState({ rules: newRules });
+    }
+
+    async handleEdit(e) {
+        let id = $(e.target).data('id');
+        this.setState({ edit: true, id });
+        setRules(rules,false);
+
+        let data = await find(id);
+        $('.modal').find('.modal-title').text('Editar usuario');
+        $('.modal').find('#name').val(data.name);
+        $('.modal').find('#email').val(data.email);
+        $('.modal').find('#password').val(data.password);
+        $('.modal').modal('toggle');
+    }
+
+    handleDelete(e) {
+        let id = $(e.target).data('id');
+        let res = confirm('¿¿Estas seguro de eliminar este usuario?');
+        if (res) {
+            destroy(id).then(data => {
+                if (data.success) {
+                    this.getUsers();
+                    toastr.success('', data.message, {
+                        closeButton: true
+                    });
+                }else {
+                    toastr.error('', data.message, {
+                        closeButton: true
+                    });
+                }
+            })
+        }
+    }
+
+    handleModal() {
+        $('#form').trigger('reset');
+        setRules(rules);
+
+        $('.modal').find('.modal-title').text('Agregar usuario');
+        $('.modal').modal('toggle');
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        if (formValid(rules)) {
+            if (this.state.edit) {
+                let data = await update(e.target, this.state.id);
+                if (data.success) {
+                    this.getUsers();
+                    $('.modal').modal('toggle');
+                    toastr.success('', data.message, {
+                        closeButton: true
+                    });
+                }else{
+                    this.setState({message: data.errors.name})
+                }
+            } else {
+                store(e.target).then(data => {
+                    if (data.success) {
+                        this.getUsers();
+                        $('.modal').modal('toggle');
+                        toastr.success('', data.message, {
+                            closeButton: true
+                        });
+                    }else{
+                        this.setState({message: data.errors.name})
+                    }
+                });
+            }
+        }else{
+            this.setState({message:'Por favor completa el formulario'});
+        }
+
+    }
+
     componentDidMount() {
         this.getUsers();
+        this.getRoles();
     }
     render() {
         if (!this.state.users) {
@@ -25,6 +124,7 @@ class Users extends Component {
                 <div className="row">
                     <div className="col">
                         <h3>Usuarios</h3>
+                        <a href="#" onClick={this.handleModal}><i className="fa fa-plus" aria-hidden="true"></i> Agregar <span className="d-none d-md-inline ">nuevo usuario</span></a>
                     </div>
                 </div>
                 <div className="row">
@@ -45,6 +145,89 @@ class Users extends Component {
                             </div>
                         )}
 
+                </div>
+                <div className="modal fade" data-backdrop="static" tabIndex="-1" role="dialog">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <form id="form" onSubmit={this.handleSubmit}>
+                                        {this.state.message ? (
+
+                                        <div className="alert alert-info" role="alert">
+                                            <span><i className="fa fa-info-circle" aria-hidden="true"></i>{this.state.message}</span>
+                                        </div>
+                                        ):(
+                                            <div></div>
+                                        )}
+                                    <div className="form-group">
+                                        <label htmlFor="">Nombre</label>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            id="name"
+                                            className={rules.name.isInvalid && rules.name.message != '' ? 'form-control is-invalid' : 'form-control'}
+                                            onInput={this.handleInput}
+                                        />
+                                        <div className="invalid-feedback">
+                                            {rules.name.isInvalid && rules.name.message != '' ? rules.name.message : ''}
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="">Correo electronico</label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            className={rules.email.isInvalid && rules.email.message != '' ? 'form-control is-invalid' : 'form-control'}
+                                            onInput={this.handleInput}
+                                        />
+                                        <div className="invalid-feedback">
+                                            {rules.email.isInvalid && rules.email.message != '' ? rules.email.message : ''}
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="">Contraseña</label>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            className={rules.password.isInvalid && rules.password.message != '' ? 'form-control is-invalid' : 'form-control'}
+                                            onInput={this.handleInput}
+                                        />
+                                        <div className="invalid-feedback">
+                                            {rules.password.isInvalid && rules.password.message != '' ? rules.password.message : ''}
+                                        </div>
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="password-confirm">Confirmar contraseña</label>
+                                        <input id="password-confirm" type="password" className="form-control" name="password_confirmation" required />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label>Asigne un rol</label>
+                                        {this.state.roles.map(rol => (
+                                            <div key={rol.id} className="form-check ">
+                                                <input className="form-check-input" type="radio" id="rol" name="rol" value={rol.id} />
+                                                <label className="form-check-label">{rol.name}</label>
+                                            </div>
+                                        ))}
+
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                <button type="submit" form="form" className="btn btn-primary">Guardar</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         )
