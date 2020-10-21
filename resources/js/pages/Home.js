1933fc25f94361933beeb1317a362f4c4665457f
Juan Pablo <juanpablotrejoscolombia@gmail.com>
2020-10-21 18:24:16 -0500

    home base

--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -1,49 +1,68 @@
 import React, {Component} from 'react';
+import { getAllCommittes, getAllStimulus, getAllSanction } from '../containers/User';
+import LineChart from '../components/LineChart';
+import DoughnutChart from '../components/DoughnutChart';
 
 class Home extends Component{
     constructor(props){
         super(props);
         this.state = {
-            committes: null,
-            id: null,
-            message: null,
-
+            allCommittes: [],
+            allStimulus: [],
+            allSanctions: [],
         }
     }
 
+    async getCommittes() {
+        let data = await getAllCommittes();
+        this.setState({ allCommittes: data });
+    }
+
+    async getStimulus() {
+        let data = await getAllStimulus();
+        this.setState({ allStimulus: data });
+    }
+
+    async getSanction() {
+        let data = await getAllSanction();
+        this.setState({ allSanctions: data });
+    }
+
     componentDidMount() {
-        // this.getCommites();
+        this.getCommittes();
+        this.getStimulus();
+        this.getSanction();
     }
 
     render(){
         return (
             <>
-                <div className="row">
-                        <div className="col">
-                                <h3>Comites agregados recientemente</h3>
-                        </div>
+            <div className="row mt-3">
+                <div className="col-5">
+                    <LineChart 
+                    label={'Aprendices en comite'}
+                    color={'rgba(255, 99, 132, 0.2)'}
+                    />
                 </div>
-                <div className="row mt-3">
-                    
+                <div className="col-5">
+                    <DoughnutChart
+                    label=""
+                    />
                 </div>
-                <div className="row">
-                        <div className="col-sm-12 col-md-6 col-lg-6 mt-3">
-                                <h3>Aprendices recientes en comites</h3>
-
-                                <ul className="list-group ml-5 mr-5 mt-2">
-                                    <li className="alert alert-primary">Juan carlos marin soto <span className="alert-warning"> ID 1835415 </span>  </li>
-
-                                    <li className="alert alert-primary">Roberto carlos muñoz <span className="alert-warning"> ID 1835274 </span> </li>
-
-                                    <li className="alert alert-primary">Santiago robledo <span className="alert-warning"> ID 1748562 </span>  </li>
-                                </ul>
-                        </div>
-                        <div className="col-sm-12 col-md-6 col-lg-6 mt-3">
-                                <h3>Grupos</h3>
-
-                                <canvas id="myChart" width="400" height="400"></canvas>
-                        </div>
+            </div>
+            <div className="row mt-5">
+                <div className="col-5">
+                    <DoughnutChart
+                    label=""
+                    />
                 </div>
+                <div className="col-5">
+                    <LineChart
+                    label={'Aprendices por estimulos'}
+                    color={'rgba(54, 162, 235, 0.2)'}
+                    />
+                </div>
+            </div>
             </>
         )
     }
